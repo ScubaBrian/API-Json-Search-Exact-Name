7860a9ff370e250f333ccf7f903d8a4ca0706b09
Brian Goers <brian_goers@belk.com>
2016-02-15 10:34:13 -0500

    Initial Commit for .js file

---
+++ api_search.js
@@ -0,0 +1,36 @@
+function uploadMode(uName){
+	var nameInput = $('#upload-name').val().toUpperCase();
+	console.log(uName);
+	if (/[a-zA-Z0-9\s]+$/.test(nameInput) && /[a-zA-Z0-9\s]+$/.test(descriptionInput) == true ){ 	
+	(function(){        
+	    $.ajax({
+	        url: 'YOUR API URL',
+	        type: 'GET',	        
+	        cache: false,
+	        dataType: 'jsonp',
+	        processData: false, // Don't process the files
+	        contentType: false, // Set content type to false as jQuery will tell the server its a query string request
+	        success: function(data, textStatus, jqXHR){	        
+                var bcArray = data.items;            
+                var bcEntry = bcArray.filter(function (bcEntry) {
+				    var entryReturn = bcEntry.name.toUpperCase() == nameInput;				    
+				    return entryReturn;						
+				})[0];		            
+				if(bcEntry === undefined){
+					alert("Sorry, this word or description cannot be found.")
+				} else {
+					console.log( bcEntry );				 
+				}		         
+	        },
+	        error: function(jqXHR, textStatus, errorThrown)
+	        {	          
+	            console.log('ERRORS: ' + textStatus);	            
+	        }
+	    });
+    })();  
+	} else if(nameInput == "" || descriptionInput == ""){
+		confirm("Please enter the required information.");
+	} else {
+	    confirm("Please use alphanumeric characters only.");		    
+	}			
+};
